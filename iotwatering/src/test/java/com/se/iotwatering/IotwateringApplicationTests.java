ff62337067042dccaf18c9da1a08a66974f0a6e9
Tu Van Nguyen Anh Quan <tvnaquan@gmail.com>
2025-02-23 18:01:32 +0700

    Init project and add .gitignore to exclude IDE and build files

---
+++ iotwatering/src/test/java/com/se/iotwatering/IotwateringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se.iotwatering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotwateringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
