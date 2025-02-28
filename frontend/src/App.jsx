191473051ca5a5b9c2200c5da1d445d246544d38
chindang5504 <chinhtran5504@gmail.com>
2025-02-28 22:31:05 +0700

    fix endpoint api and create protected route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,30 @@
 
 import { ToastContainer } from 'react-toastify'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate, Outlet } from 'react-router-dom'
 import 'react-toastify/dist/ReactToastify.css'
 import Auth from './pages/Auth/Auth.jsx'
 import HomePage from './pages/HomePage/HomePage.jsx'
 import Dashboard from './pages/Dashboard/Dashboard.jsx'
-import Register from './pages/Auth/Register.jsx'
+import { useSelector } from 'react-redux'
+import { selectCurrentUser } from './redux/Slices/userSlice.js'
 
+const ProtectedRoute = ({ user }) => {
+  if (!user) return <Navigate to='/Login' replace={true} />
+  return <Outlet />
+}
 
 function App() {
-
+  const currentUser = useSelector(selectCurrentUser)
 
   return (
     <div className="bg-[#EEEEEE]" style={{ height: '100vh' }}>
       <div>
         <ToastContainer />
         <Routes>
-          <Route path='/' element={<HomePage/>}/>
-          <Route path='/Dashboard/:DashboardId' element={<Dashboard/>}/>
+          <Route element={<ProtectedRoute user={currentUser}/>}>
+            <Route path='/' element={<HomePage/>}/>
+            <Route path='/Dashboard/:DashboardId' element={<Dashboard/>}/>
+          </Route>
           <Route path='/Signup' element={<Auth/>}/>
           <Route path='/Login' element={<Auth/>}/>
         </Routes>
